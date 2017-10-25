c298cd044c4d794b78d78dbd961e90908ef9aea8
jack <hao3304@qq.com>
2017-10-25 10:30:45 +0800

    加入指标

--- src/module/filter.js
+++ src/module/filter.js
@@ -1,7 +1,7 @@
 import vue from 'vue'
 
 vue.filter('formatNum',(num)=>{
-  if(!/^(\+|-)?(\d+)(\.\d+)?$/.test(num)){  return   'loading';}
+  if(!/^(\+|-)?(\d+)(\.\d+)?$/.test(num)){  return   '-';}
   var   a   =   RegExp.$1,   b   =   RegExp.$2,   c   =   RegExp.$3;
   var   re   =   new   RegExp().compile("(\\d)(\\d{3})(,|$)");
   while(re.test(b))   b   =   b.replace(re,   "$1,$2$3");
